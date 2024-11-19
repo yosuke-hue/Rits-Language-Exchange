3340d19e5bf95b63c8582c78ce733e2a66029f8f
BKCihouse <113819059+yosuke-hue@users.noreply.github.com>
2024-11-19 15:39:35 +0900

    tata

--- app/api/post/[bbsId]/route.ts
+++ app/api/post/[bbsId]/route.ts
@@ -3,9 +3,9 @@ import prisma from "../../../../lib/prismaClient";
 
 export async function GET(
     req: Request, 
-    {params}: {params: {bbsId:string } }
+    {params}: {params: Promise<{bbsId:string }> }
 ) {
-    const bbsId = params.bbsId;
+    const bbsId = (await params).bbsId;
     const bbsDetailData = await prisma.post.findUnique({
         where: {
         id: parseInt(bbsId),
--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../lib/prismaClient";
 
-export async function GET() {
+export async function GET(req: Request) {
     const allBBSPosts = await prisma.post.findMany();
     return NextResponse.json(allBBSPosts);
 };
 
-export async function POST(request: Request) {
-    const { username, title, content } = await request.json();
+export async function POST(req: Request) {
+    const { username, title, content } = await req.json();
 
     const post = await prisma.post.create({
         data: {
